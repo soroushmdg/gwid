dbec58c4573aeaf92f7e93078440cd7009523cfd
soroushmdg <soroush.mdehkordi@gmail.com>
2024-06-05 23:16:08 -0500

    1- -log10 default 2- inherites function

--- R/plot.R
+++ R/plot.R
@@ -21,7 +21,7 @@
 #'genome_data_file <- paste0(tempdir(),"//chr3.gds")
 #'phase_data_file <- paste0(tempdir(),"//chr3.vcf")
 #'case_control_data_file <- paste0(tempdir(),"//case-cont-RA.withmap.Rda")
-#'# case-control data
+#' # case-control data
 #'case_control <- gwid::case_control(case_control_rda = case_control_data_file)
 #'names(case_control) #cases and controls group
 #'summary(case_control) # in here, we only consider cases,cont1,cont2,cont3 groups in the study
@@ -31,7 +31,7 @@
 #'caco = case_control,gwas_generator = TRUE)
 #'class(snp_data_gds)
 #'names(snp_data_gds)
-#'head(snp_data_gds$snps) # it has information about counts of minor alleles in each location.
+#'head(snp_data_gds$snps)
 #'# read haplotype data (output of beagle)
 #'haplotype_data <- gwid::build_phase(phased_vcf = phase_data_file,caco = case_control)
 #'class(haplotype_data)
@@ -334,7 +334,7 @@ plot.gwas <- function(x, y = NA, title = "number of snps", ...) {
 #'}
 #' @export
 plot.test_snps <- function(x, y = NA, title, snp_start, snp_end, ly =TRUE,
-                           line_size = .6,log_transformation = FALSE,
+                           line_size = .6,log_transformation = TRUE,
                            QQplot = FALSE,...) {
   snp_pos <- value <- NULL
   if (!missing(snp_start) & !missing(snp_end)) {
@@ -366,8 +366,12 @@ plot.test_snps <- function(x, y = NA, title, snp_start, snp_end, ly =TRUE,
     p <- ggplot2::ggplot(x, ggplot2::aes(x = snp_pos, y = value)) +
       ggplot2::geom_line(ggplot2::aes(color = case_control), size = line_size) +
       ggplot2::scale_x_continuous("snp position",
-                                  labels = paste0(round(quantile(x$snp_pos, seq(0, 1, length.out = 5)) / 10^6), "M"),
-                                  breaks = quantile(x$snp_pos, seq(0, 1, length.out = 5))
+                                  labels = paste0(round(
+                                    quantile(x$snp_pos,
+                                             seq(0, 1, length.out = 5)) / 10^6),
+                                    "M"),
+                                  breaks = quantile(x$snp_pos,
+                                                    seq(0, 1, length.out = 5))
       ) +
 
       ggplot2::labs(y = "p_values",
@@ -385,7 +389,8 @@ plot.test_snps <- function(x, y = NA, title, snp_start, snp_end, ly =TRUE,
       fill = "case_control"
     )
   }
-  p <- p + ggplot2::theme(legend.title = ggplot2::element_text(size = 10)) + ggplot2::theme(
+  p <- p + ggplot2::theme(legend.title = ggplot2::element_text(size = 10)) +
+    ggplot2::theme(
     axis.ticks = ggplot2::element_line(colour = "black"),
     panel.grid.major = ggplot2::element_line(
       colour = "antiquewhite",
@@ -483,32 +488,47 @@ plot.test_snps <- function(x, y = NA, title, snp_start, snp_end, ly =TRUE,
 #'nwin = 1, type = "version1",ly = FALSE)
 #'}
 #' @export
-plot.haplotype_structure_frequency <- function(x, y = NA, type = c("version1", "version2"), nwin, ly = TRUE, line_size=.6,...) {
+plot.haplotype_structure_frequency <- function(x, y = NA,
+                                               type = c("version1", "version2"),
+                                               nwin,
+                                               ly = TRUE, line_size=.6,...) {
   type <- match.arg(type)
   structures <- window_number <- n <- NULL
   if (any(!is.na(y))){
     x <- x[case_control %in% y]
   }
   if (type == "version1") {
-    p <- ggplot2::ggplot(x[window_number == nwin, ], ggplot2::aes(x = structures, y = n, frame = window_number)) +
-      ggplot2::geom_line(ggplot2::aes(color = case_control, group = case_control), size = line_size) +
-      ggplot2::scale_x_discrete("haplotypes", label = function(x) strtrim(x, 12)) +
-      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+    p <- ggplot2::ggplot(x[window_number == nwin, ],
+                         ggplot2::aes(x = structures, y = n,
+                                      frame = window_number)) +
+      ggplot2::geom_line(ggplot2::aes(color = case_control,
+                                      group = case_control), size = line_size) +
+      ggplot2::scale_x_discrete("haplotypes",
+                                label = function(x) strtrim(x, 12)) +
+      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90,
+                                                         vjust = 0.5,
+                                                         hjust = 1)) +
       ggplot2::labs(
         title = "haplotype frequency version 1", y = "number of structures",
         fill = "case_control"
       )
   } else {
-    p <- ggplot2::ggplot(x[window_number == nwin, ], ggplot2::aes(x = case_control, y = n, frame = window_number)) +
-      ggplot2::geom_line(ggplot2::aes(color = structures, group = structures), size = line_size) +
+    p <- ggplot2::ggplot(x[window_number == nwin, ],
+                         ggplot2::aes(x = case_control, y = n,
+                                      frame = window_number)) +
+      ggplot2::geom_line(ggplot2::aes(color = structures, group = structures),
+                         size = line_size) +
       ggplot2::scale_x_discrete("structures") +
-      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90,
+                                                         vjust = 0.5,
+                                                         hjust = 1)) +
       ggplot2::labs(
         title = "haplotype frequency version 2", y = "number of structures",
         fill = "case_control"
       )
   }
-  p <- p + ggplot2::theme(legend.title = ggplot2::element_text(size = 10)) + ggplot2::theme(
+  p <- p + ggplot2::theme(legend.title = ggplot2::element_text(size = 10)) +
+    ggplot2::theme(
     axis.ticks = ggplot2::element_line(colour = "black"),
     panel.grid.major = ggplot2::element_line(
       colour = "antiquewhite",
@@ -549,18 +569,22 @@ plot.haplotype_structure_frequency <- function(x, y = NA, type = c("version1", "
 #' @param x an object of class haplotype_frequency
 #' @param y default value is `NA`, if specified it should be a vector of names of
 #' subject groups i.e. `y = c("case","control")`
-#' @param plot_type either \dQuote{result_snps} or \dQuote{"haplotype_structure_frequency"}
-#' @param type either \dQuote{version1} or \dQuote{version2} when plot_type is \dQuote{"haplotype_structure_frequency"}
+#' @param plot_type either \dQuote{result_snps}
+#' or \dQuote{"haplotype_structure_frequency"}
+#' @param type either \dQuote{version1} or \dQuote{version2}
+#' when plot_type is \dQuote{"haplotype_structure_frequency"}
 #' @param nwin window number
-#' @param ly if TRUE, we have a plotly object and if it is false plot is going to be
-#' a ggplot object.
+#' @param ly if TRUE, we have a plotly object and if it is false plot
+#' is going to be a ggplot object.
 #' @param title title of the plot.
 #' @param line_size geom_line size
 #' @param ... optional argument of \code{plot}
-#' @return an interactive line plot of haplotype_frequency objects for each case control subjects.
+#' @return an interactive line plot of haplotype_frequency objects for
+#' each case control subjects.
 #'@examples
 #'\donttest{
-#'piggyback::pb_download(repo = "soroushmdg/gwid",tag = "v0.0.1",dest = tempdir())
+#'piggyback::pb_download(repo = "soroushmdg/gwid",tag = "v0.0.1",
+#'dest = tempdir())
 #'ibd_data_file <- paste0(tempdir(),"//chr3.ibd")
 #'genome_data_file <- paste0(tempdir(),"//chr3.gds")
 #'phase_data_file <- paste0(tempdir(),"//chr3.vcf")
@@ -568,7 +592,7 @@ plot.haplotype_structure_frequency <- function(x, y = NA, type = c("version1", "
 #'# case-control data
 #'case_control <- gwid::case_control(case_control_rda = case_control_data_file)
 #'names(case_control) #cases and controls group
-#'summary(case_control) # in here, we only consider cases,cont1,cont2,cont3 groups in the study
+#'summary(case_control)
 #'case_control$cases[1:3] # first three subject names of cases group
 #'# read SNP data (use SNPRelate to convert it to gds) and count number of minor alleles
 #'snp_data_gds <- gwid::build_gwas(gds_data = genome_data_file,
@@ -602,16 +626,25 @@ plot.haplotype_structure_frequency <- function(x, y = NA, type = c("version1", "
 #'nwin = 1, type = "version1",ly = FALSE)
 #'}
 #' @export
-plot.haplotype_frequency <- function(x, y = NA, plot_type = c("haplotype_structure_frequency", "result_snps"), type = c("version1", "version2"),ly=TRUE, nwin, title,line_size = .6, ...) {
+plot.haplotype_frequency <- function(x,
+                                     y = NA,
+                                     plot_type =
+                                       c("haplotype_structure_frequency",
+                                         "result_snps"),
+                                     type = c("version1", "version2"),ly=TRUE,
+                                     nwin, title,line_size = .6, ...) {
   plot_type <- match.arg(plot_type)
   if (plot_type == "haplotype_structure_frequency") {
     type <- match.arg(type)
-    plot(x[[which(unlist(lapply(x, inherits, "haplotype_structure_frequency")))]], y, type = type, nwin = nwin,ly = ly,line_size=line_size)
+    plot(x[[which(unlist(lapply(x, inherits, "haplotype_structure_frequency")))]],
+         y, type = type, nwin = nwin,ly = ly,line_size=line_size)
   } else {
     if (!missing(title)) {
-      plot(x[[which(unlist(lapply(x, inherits, "result_snps")))]], y, title = title, ly=ly, ...)
+      plot(x[[which(unlist(lapply(x, inherits, "result_snps")))]], y,
+           title = title, ly=ly, ...)
     } else {
-      plot(x[[which(unlist(lapply(x, inherits, "result_snps")))]], y, ly = ly, ...)
+      plot(x[[which(unlist(lapply(x, inherits, "result_snps")))]],
+           y, ly = ly, ...)
     }
   }
 }
@@ -627,10 +660,15 @@ qqunif_plot<-function(pvalues,
 
 
   #error checking
-  if (length(pvalues)==0) stop("pvalue vector is empty, can't draw plot")
-  if(!(class(pvalues)=="numeric" ||
-       (class(pvalues)=="list" && all(sapply(pvalues, class)=="numeric"))))
-    stop("pvalue vector is not numeric, can't draw plot")
+  if (length(pvalues)==0) {
+    stop("pvalue vector is empty, can't draw plot")}
+  # if(!(class(pvalues)=="numeric" ||
+  #      (class(pvalues)=="list" && all(sapply(pvalues, class)=="numeric")))){
+  #   stop("pvalue vector is not numeric, can't draw plot")}
+
+  if(!(inherits(pvalues,what = "numeric") ||
+       (inherits(pvalues,"list") && all(sapply(pvalues, inherits,"numeric"))))){
+    stop("pvalue vector is not numeric, can't draw plot")}
   if (any(is.na(unlist(pvalues)))) stop("pvalue vector contains NA values, can't draw plot")
   if (already.transformed==FALSE) {
     if (any(unlist(pvalues)==0)) stop("pvalue vector contains zeros, can't draw plot")
